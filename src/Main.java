96be762c93934a017dfc2d2667968e691860f75a
Alban <albancousin12@gmail.com>
2022-12-14 10:48:51 +0100

    main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+package src;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+    
+    public Main() 
+    {
+
+    }
+    public static void main(String [] args)
+    {
+
+    }
+}
