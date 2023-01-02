b6c4577ff82cb356ec4f79960efacf623f764137
AdrienRov <54404616+AdrienRov@users.noreply.github.com>
2023-01-02 22:54:18 +0100

    maj compilelist

--- lstart.sh
+++ lstart.sh
@@ -1,2 +1,2 @@
-javac -d ./class @compile.list
+javac -d ./class @compile.list -encoding utf8
 java -cp ./class src.Controleur
--- wstart.bat
+++ wstart.bat
@@ -1,2 +1,2 @@
-javac -d ./class "@compile.list"
-java -cp ./class src.Controleur
+javac -d ./class "@compile.list" -encoding utf8
+java -cp ./class src.Controleur 
